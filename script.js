d434ab7b54e116524b96e1862ff2891bb801d180
Glitch (scapetrial) <none>
2021-05-04 10:09:39 +0000

    💪🎆 Updated with Glitch

--- script.js
+++ script.js
@@ -1,94 +1,130 @@
 const config = {
-  time: {
-    "24hour": true,
-    show: true
+  style: {
+    background:
+      "https://cdn.glitch.com/fbcc75ee-28e3-462b-9d78-8dd9e7264ccd%2Fken-cheung-KonWFWUaAuk-unsplash.jpg?v=1620121865175",
+    round: false,
+    css: ""
   },
-  date: {
-    show: false
-  },
-  weather: {
-    show: true,
-    location: "auto"
-  },
-  search: {
-    show: true,
-    engine: "https://www.bing.com/search",
-    placeholder: false
-  },
-  bookmarks: {
-    show: true,
-    items: [
-      "https://notion.so",
-      "https://youtube.com",
-      "https://mail.google.com"
-    ]
+  modules: {
+    time: {
+      show: true,
+      "24hour": false,
+      ampm: true
+    },
+    weather: {
+      show: true,
+      units: "metric",
+      location: false
+    },
+    search: {
+      show: false,
+      engine: "https://www.google.com/search",
+      placeholder: false
+    },
+    bookmarks: {
+      show: true,
+      items: [
+        "https://notion.so",
+        "https://youtube.com",
+        "https://mail.google.com"
+      ]
+    }
   }
 };
 
-// Time
-document.getElementById("time").innerText = new Date().toLocaleString([], {
-  hour: "2-digit",
-  minute: "2-digit"
-});
-setInterval(() => {
-  document.getElementById("time").innerText = new Date().toLocaleString([], {
-    hour: "2-digit",
-    minute: "2-digit"
-  });
-}, 1000);
+// Style
 
-// Date
+// Time
+if (config.modules.time.show === true) {
+  const time = document.getElementById("time");
+
+  time.style.display = "";
+
+  const options = config.modules.time["24hour"]
+    ? { hour: "numeric", minute: "numeric", hour12: false }
+    : {
+        hour: "numeric",
+        minute: "numeric",
+        hour12: true
+      };
+
+  time.innerText = config.modules.time.ampm
+    ? new Date().toLocaleString("en-GB", options)
+    : new Date().toLocaleString("en-GB", options).replace(/am|pm| /gi, "");
+
+  setInterval(() => {
+    time.innerText = config.modules.time.ampm
+      ? new Date().toLocaleString("en-GB", options)
+      : new Date().toLocaleString("en-GB", options).replace(/am|pm| /gi, "");
+  }, 1000);
+}
 
 // Weather
-const icon = document.getElementById("icon");
-const type = document.getElementById("type");
-const temp = document.getElementById("temp");
+if (config.modules.weather.show === true) {
+  const weather = document.getElementById("weather");
+  const icon = document.getElementById("icon");
+  const type = document.getElementById("type");
+  const temp = document.getElementById("temp");
 
-fetch("https://ipwhois.app/json/")
-  .then(ipdata => ipdata.json())
-  .then(ipjson => {
-    var units = "metric";
-    if (ipjson.country_code === "US") units = "imperial";
-    fetch(
-      `https://api.openweathermap.org/data/2.5/weather?lang=EN&lat=${ipjson.latitude}&lon=${ipjson.longitude}&units=${units}&appid=8e586fc94a1f3326672f6733aa38fd55`
-    )
-      .then(weatherdata => weatherdata.json())
-      .then(weatherjson => {
-        icon.setAttribute("class", "wi wi-owm-" + weatherjson.weather[0].id);
-        type.innerText = weatherjson.weather[0].main;
-        temp.innerText = Math.round(weatherjson.main.temp) + "°";
-      });
-  });
+  weather.style.display = "";
+
+  const loadWeather = async function() {
+    const location = await (await fetch("https://ip2tz.isthe.link/")).json();
+
+    var units = config.modules.weather.units;
+
+    const weatherData = await (await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?lang=EN&lat=${location.latitude}&lon=${location.longitude}&units=${units}&appid=8e586fc94a1f3326672f6733aa38fd55`
+    )).json();
+
+    icon.setAttribute("class", "wi wi-owm-" + weatherData.weather[0].id);
+    type.innerText = weatherData.weather[0].main;
+    temp.innerText = Math.round(weatherData.main.temp) + "°";
+  };
+
+  loadWeather();
+}
 
 // Search
-const search = document.getElementById("search");
-const input = search.elements.q;
-const placeholders = [
-  "What is the weather?",
-  "How tall is the empire state building?",
-  "Why is the sky blue?",
-  "How much is Bitcoin worth?",
-  "What is the internet?",
-  "What is Elon Musk's net worth?",
-  "What's my IP?",
-  "What time is it in Beijing?",
-  "How old is Joe Biden?",
-  "What is Reddit?"
-];
-const placeholder =
-  placeholders[Math.floor(Math.random() * placeholders.length)];
-search.setAttribute("action", config.search.engine);
-if (config.search.placeholder !== false) {
-  input.setAttribute("placeholder", config.search.placeholder);
-} else {
-  input.setAttribute("placeholder", placeholder);
+if (config.modules.search.show === true) {
+  const search = document.getElementById("search");
+  const input = search.elements.q;
+
+  search.style.display = "";
+
+  const placeholders = [
+    "What is the weather?",
+    "How tall is the empire state building?",
+    "Why is the sky blue?",
+    "How much is Bitcoin worth?",
+    "What is the internet?",
+    "What is Elon Musk's net worth?",
+    "What's my IP?",
+    "What time is it in Beijing?",
+    "How old is Joe Biden?",
+    "What is Reddit?",
+    "What is COVID-19?",
+    "How to code"
+  ];
+  const placeholder =
+    placeholders[Math.floor(Math.random() * placeholders.length)];
+
+  search.setAttribute("action", config.modules.search.engine);
+  if (config.modules.search.placeholder !== false) {
+    input.setAttribute("placeholder", config.modules.search.placeholder);
+  } else {
+    input.setAttribute("placeholder", placeholder);
+  }
 }
 
 // Bookmarks
-const bookmarks = document.getElementById("bookmarks");
+if (config.modules.bookmarks.show === true) {
+  const bookmarks = document.getElementById("bookmarks");
 
-config.bookmarks.items.forEach(bookmark => {
-  bookmarks.innerHTML += `
+  bookmarks.style.display = "";
+
+  config.modules.bookmarks.items.forEach(bookmark => {
+    bookmarks.innerHTML += `
   <a href="${bookmark}">
     <img 
       src="https://favicon.splitbee.io/?url=${bookmark}"
@@ -97,4 +133,5 @@ config.bookmarks.items.forEach(bookmark => {
     />
   </a>
   `;
-});
+  });
+}
